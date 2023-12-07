54ac2ae896f58424047c5421f864bcce0fcd645a
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-12-07 21:29:40 +0600

    models and forms done

--- category/models.py
+++ category/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from category.models import Task
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    tasks = models.ManyToManyField(Task)
